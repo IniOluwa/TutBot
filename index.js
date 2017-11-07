64f1f02ede6e76f6c4abf10a24e184b9da61153a
IniOluwa <inioluwafageyinbo@gmail.com>
2017-11-07 23:03:23 +0100

    Rewrote Request To Attempt Sending Messages.

--- index.js
+++ index.js
@@ -46,7 +46,23 @@ app.post('/webhook', (req, res) => {
       };
 
       // Returns a response to the user
-      request.post('https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN, userResponse, (error, response, body) => {
+
+      // request.post('https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN, userResponse, (error, response, body) => {
+      //   // If an error occured
+      //   console.log('Error: ', error);
+      //
+      //   // Check response StatusCode code
+      //   console.log('StatusCode: ', response, response.StatusCode);
+      //
+      //   // Response Body
+      //   console.log('Body: ', response.body);
+      // });
+
+      request({
+        method: 'POST',
+        uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN,
+        json: userResponse
+      }, (error, response, body) => {
         // If an error occured
         console.log('Error: ', error);
 
