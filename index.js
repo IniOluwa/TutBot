83301bb6f6a70ac45e437803c5900cd4604403aa
IniOluwa <inioluwafageyinbo@gmail.com>
2017-11-08 16:01:48 +0100

    Generating Name For User & Saving User To Database.

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const
   mongoose = require('mongoose');
 
 // Database Connection
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(process.env.MONGODB_URI);
 let db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error: '));
@@ -64,97 +63,101 @@ let newUser = (userOriginalName, userBotGeneratedName, userSenderId, userUserLoc
   });
 };
 
-// // Sets server port and logs message on success
-// app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
-//
-// // Default Application Response
-// app.get('/', (req, res) => {
-//   res.send("You Are Very Welcome! :) ");
-// });
-//
-// app.post('/webhook', (req, res) => {
-//
-//   // Define body
-//   let body = req.body;
-//
-//   // Checks this is an event from a page subscription
-//   if (body.object === 'page') {
-//
-//     // Iterates over each entry - there may be multiple if batched
-//     body.entry.forEach(function(entry) {
-//
-//       // Gets the message. entry.messaging is an array, but
-//       // will only ever contain one message, so we get index 0
-//       let webhookEvent = entry.messaging[0];
-//       console.log(webhookEvent);
-//
-//       // Recipient's Id
-//       let recipientId = entry.messaging[0].sender.id;
-//
-//       // Recipient's Message
-//       let recipientMessage = entry.messaging[0].message.text;
-//
-//       // User's Response
-//       let userResponse = {
-//         'recipient': {
-//           'id': recipientId,
-//         },
-//         'message': {
-//           'text': recipientMessage,
-//         }
-//       };
-//
-//       // Returns a response to the user
-//       request({
-//         method: 'POST',
-//         uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN,
-//         json: userResponse
-//       }, (error, response, body) => {
-//         // If an error occured
-//         console.log('Error: ', error);
-//
-//         // Check response StatusCode code
-//         console.log('StatusCode: ', response, response.StatusCode);
-//
-//         // Response Body
-//         console.log('Body: ', response.body);
-//       });
-//
-//     });
-//
-//     // Returns a '200 OK' response to all requests
-//     res.status(200).send('EVENT_RECEIVED');
-//
-//   } else {
-//     // Returns a '404 Not Found' if event is not from a page subscription
-//     res.sendStatus(404);
-//   }
-// });
-//
-// // Adds support for GET requests to our webhook
-// app.get('/webhook', (req, res) => {
-//
-//   // Your verify token. Should be a random string.
-//   let VERIFY_TOKEN = "TuttBottVerified"
-//
-//   // Parse the query params
-//   let mode = req.query['hub.mode'];
-//   let token = req.query['hub.verify_token'];
-//   let challenge = req.query['hub.challenge'];
-//
-//   // Checks if a token and mode is in the query string of the request
-//   if (mode && token) {
-//
-//     // Checks the mode and token sent is correct
-//     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-//
-//       // Responds with the challenge token from the request
-//       console.log('WEBHOOK_VERIFIED');
-//       res.status(200).send(challenge);
-//
-//     } else {
-//       // Responds with '403 Forbidden' if verify tokens do not match
-//       res.sendStatus(403);
-//     }
-//   }
-// });
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
+
+// Default Application Response
+app.get('/', (req, res) => {
+  res.send("You Are Very Welcome! :) ");
+});
+
+app.post('/webhook', (req, res) => {
+
+  // Define body
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+
+      // Recipient's Id
+      let recipientId = entry.messaging[0].sender.id;
+
+      // Recipient's Message
+      let recipientMessage = entry.messaging[0].message.text;
+
+      // Get & Save Available User
+      newUser('User\'sFacebookName', generateName(), entry.messaging[0].sender.id, 'User\'sLocale', true);
+
+      // User's Response
+      let userResponse = {
+        'recipient': {
+          'id': recipientId,
+        },
+        'message': {
+          'text': newUser.botGeneratedName ? "Your Id For This Session Is " + newUser.botGeneratedName : recipientMessage,
+        }
+      };
+
+
+      // Returns a response to the user
+      request({
+        method: 'POST',
+        uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN,
+        json: userResponse
+      }, (error, response, body) => {
+        // If an error occured
+        console.log('Error: ', error);
+
+        // Check response StatusCode code
+        console.log('StatusCode: ', response, response.StatusCode);
+
+        // Response Body
+        console.log('Body: ', response.body);
+      });
+
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "TuttBottVerified"
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
